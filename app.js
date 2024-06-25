621409076bd5976fa4aeba026d5e06f5f4458acc
Skomantas Jankevičius <skomantas.j@gmail.com>
2024-06-25 13:15:56 +0300

    fx

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const bodyParser = require('body-parser');
 const mongoose = require("mongoose");
 const app = express();
 const cors = require('cors')
+const dotenv = require('dotenv');
+
+dotenv.config({path:'config.env'});
+
 
 
 
