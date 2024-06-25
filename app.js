8d8985673d0baa4236066b9e9063ad00546d3879
Skomantas Jankevičius <skomantas.j@gmail.com>
2024-06-25 13:44:13 +0300

    added code coverage

--- app.js
+++ app.js
@@ -5,10 +5,6 @@ const bodyParser = require('body-parser');
 const mongoose = require("mongoose");
 const app = express();
 const cors = require('cors')
-const dotenv = require('dotenv');
-
-dotenv.config({path:'config.env'});
-
 
 
 
